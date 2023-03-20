49fb869bbf9c8b47416cbafae075ab2aa35e0684
israellinofaustino <israellinofaustino@hotmail.com>
2023-03-20 19:50:03 +0000

    exercice_completed

--- exercises_third_world/ex084.py
+++ exercises_third_world/ex084.py
@@ -1,18 +1,38 @@
-data = []
-ppl = []
-max_weight = 0
+temp = []
+people = []
+max_weight = min_weight = 0
 
 while True:
-    data.append(input("Name: "))
-    data.append(float(input("Weight: ")))
-    ppl.append(data[:])
-    data.clear()
+    temp.append(input("Name: "))
+    temp.append(float(input("Weight: ")))
+    if len(people) == 0:
+        max_weight = min_weight = temp[1]
+    else:
+        if temp[1] > max_weight:
+            max_weight = temp[1]
+        if temp[1] < min_weight:
+            min_weight = temp[1]
+    people.append(temp[:])
+    temp.clear()
 
     still = input("Do you want continue? [Y/N] => ").strip().upper()[0]
     if still == "N":
         break
 
 print("-=" * 25)
-print(f"Altogether you have registered {len(ppl)} people.")
-# print(ppl)
 
+print(f"Altogether you have registered {len(people)} people.")
+
+print("-=" * 25)
+
+print(f"The highest weight was{max_weight}Kg. weight of ", end='')
+for person in people:
+    if person[1] == max_weight:
+        print(f"[{person[0]}] ", end='')
+
+print()
+
+print(f"The lowest weight was {min_weight}Kg. weight of ", end='')
+for person in people:
+    if person[1] == min_weight:
+        print(f"[{person[0]}] ", end='')
