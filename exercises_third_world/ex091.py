cad2125150a3c332aa86811e3ba7c21349c8bb22
israellinofaustino <israellinofaustino@hotmail.com>
2023-04-12 21:04:07 +0100

    using itemgetter

---
+++ exercises_third_world/ex091.py
@@ -0,0 +1,21 @@
+from random import randint
+from operator import itemgetter
+from time import sleep
+
+gamers = {'gamer1': randint(1, 6), 'gamer2': randint(1, 6),
+        'gamer3': randint(1, 6), 'gamer4': randint(1, 6)}
+
+ranking = list()
+print("-=-=-=-= Dice Game -=-=-=-=")
+for k, v in gamers.items():
+    print(f'{k} got => {v}')
+    sleep(1)
+
+ranking = sorted(gamers.items(), key=itemgetter(1), reverse=True)
+
+print('-=' * 14)
+
+for i, v in enumerate(ranking):
+    print(f'{i+1}º place => {v[0]} with {v[1]}')
+    sleep(1)
+print("-=-=-=-=   END!   -=-=-=-=")
