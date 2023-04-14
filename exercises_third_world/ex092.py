6880ae1f016a6e9fdab8ec4f416267af91d4f90f
israellinofaustino <israellinofaustino@hotmail.com>
2023-04-14 22:16:10 +0100

    exercise 92 finished

--- exercises_third_world/ex092.py
+++ exercises_third_world/ex092.py
@@ -1,25 +1,15 @@
 from datetime import date
 
-
 person = dict()
 
-person['nome'] = str(input("Write a name: "))
-person['year_of_birth'] = int(input("Write your year of birth: "))
+person['name'] = str(input("Write a name: "))
+year_of_birth = int(input("Write your year of birth: "))
+person['age'] = date.today().year - year_of_birth
 person['work_card'] = int(input("Write your work permit number (0 if you don't have): "))
-person['year_of_hiring'] = int(input("Write your year of hiring (0 if you don't have): "))
-person['salary'] = float(input("Write your salary (0 if you don't have) R$"))
-
-person['age'] = date.today().year - person["year_of_birth"]
-if person['work_card'] == 0:
-    del person['year_of_birth']
-    del person['salary']
-    del person['year_of_hiring']
-    print('-=' * 30)
-    for k, v in person.items():
-        print(f"  - {k} have the value {v}")
-else:
-    del person['year_of_birth']
-    del person['year_of_hiring']
-    print('-=' * 30)
-    for k, v in person.items():
-        print(f"  - {k} have the value {v}")
+if person['work_card'] != 0:
+    person['year_of_hiring'] = int(input("Write your year of hiring: "))
+    person['salary'] = float(input("Write your salary R$"))
+    person['retirement'] = person['age'] + ((person['year_of_hiring'] + 35) - date.today().year)
+print('-=' * 30)
+for k, v in person.items():
+    print(f"  - {k} have the value {v}")
