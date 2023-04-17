1fde915cb90b55933e79e75dedad0ff7b3f4d072
israellinofaustino <israellinofaustino@hotmail.com>
2023-04-17 23:25:25 +0100

    Last exercise about dict()

---
+++ exercises_third_world/ex095.py
@@ -0,0 +1,41 @@
+players = list()
+player = dict()
+goals = list()
+
+while True:
+    player['name'] = str(input("Name of player: "))
+    many_games = int(input(f"How many games did the {player['name']} play? "))
+    for c in range(0, many_games):
+        goal = int(input(f"How many goals in match {c+1}? => "))
+        goals.append(goal)
+    player['goals'] = goals[:]
+    goals.clear()
+    player['total_goals'] = sum(player['goals'])
+    players.append(player.copy())
+
+    question = str(input("Do you want continue? [Y/N] => ")).strip().upper()[0]
+    while question != 'Y' and question != 'N':
+        print("ERROR! Please write just Y or N.")
+        question = input("Do you want continue? [Y/N]: ").strip().upper()[0]
+    if question == 'N':
+        break
+
+
+print('-=' * 30)
+print(f"{'cod':<4} {'name':<8} {'goals'} {'total goals':>38}")
+print('-' * 60)
+for i in enumerate(players):
+    print(f"{i[0]:<4} {i[1]['name']:<8} {i[1]['goals']} {i[1]['total_goals']:>25}")
+
+print('-=' * 30)
+
+while True:
+    quest = int(input("Mostrar dados de qual jogador? (999 to end) => "))
+    if quest == 999:
+        break
+    print('-=' * 20)
+    print(f" -- LEVANTAMENTO DO JOGADOR {players[quest]['name']}")
+    print('-=' * 20)
+    for position, goals in enumerate(players[quest]['goals']):
+        print(f"     No jogo {position + 1} fez {goals} goals.")
+    print('-=' * 20)
