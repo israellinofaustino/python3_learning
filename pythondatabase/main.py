e1b44937b572388609c6e2279c258da979bb6d33
israellinofaustino <israellinofaustino@hotmail.com>
2023-06-06 22:24:55 +0100

    code review and modularisation

--- pythondatabase/main.py
+++ pythondatabase/main.py
@@ -1,40 +1,21 @@
-import mysql.connector
+from packages import connection
 
 
-mydatabase = mysql.connector.connect(
-    host='localhost',
-    username='root',
-    password='',
-    database='people'
-)
-if mydatabase.is_connected() == True:
-    print('Successfully Connected!')
-else:
-    print('Something wrong happend with the database connection.')
-
-mycursor = mydatabase.cursor()
-
-person = []
+list_of_person = []
 while True:
     question = str(input("Do you want add someone? [Yes/No] => ")).strip().upper()[0]
     if question == 'N':
-        mycursor.execute("SELECT * FROM people.person")
-        result = mycursor.fetchall()
-        for i in result:
-            print(i)
-        break    
+        connection.show_data('people.person')
+        break
     name = str(input("Write your name: ")).strip()
     surname = str(input("Write your surname: ")).strip()
     email = str(input("Write your email: ")).strip()
     address = str(input("Write yout address: ")).strip()
     end = str(input("Do you want continue? [Yes/No]: ")).strip().upper()[0]
-    person.append(tuple([name, surname, email, address]))
+    list_of_person.append(tuple([name, surname, email, address]))
     if end == 'N':
         break
     else:
         continue
 
-sqlinsert = "INSERT INTO person (name, surname, email, address, date_time) VALUES (%s, %s, %s, %s, NOW())"
-mycursor.executemany(sqlinsert, person)
-mydatabase.commit()
-print(mycursor.rowcount, " rows.")
+connection.insert_data(list_of_person)
--- pythondatabase/packages/connection/__init__.py
+++ pythondatabase/packages/connection/__init__.py
@@ -1,14 +1,33 @@
 import mysql.connector
 
 
-def connection_mysql_db():
-    mydatabase = mysql.connector.connect(
-        host='localhost',
-        username='root',
-        password='',
-        database='people'
-    )
-    if mydatabase.is_connected() == True:
-        print('Successfully Connected!')
-    else:
-        print('Something wrong happend with the database connection.')
+mydatabase = mysql.connector.connect(
+    host='localhost',
+    username='root',
+    password='',
+    database='people'
+)
+if mydatabase.is_connected() == True:
+    print('Successfully Connected!')
+else:
+    print('Something wrong happend with the database connection.')
+
+
+def committ():
+    return mydatabase.commit()
+
+
+def show_data(table_name):
+    mycursor = mydatabase.cursor() 
+    mycursor.execute(f"SELECT * FROM {table_name}")
+    result = mycursor.fetchall()
+    for i in result:
+        print(i)
+
+
+def insert_data(list_data):
+    mycursor = mydatabase.cursor() 
+    sqlinsert = "INSERT INTO person (name, surname, email, address, date_time) VALUES (%s, %s, %s, %s, NOW())"
+    mycursor.executemany(sqlinsert, list_data)
+    committ()
+
